c1222ce547f9550b105036174a23c50323cfedb3
thong <thongviet@gmail.com>
2024-11-24 14:04:48 +0700

    ok sugar

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, 'documents')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'documents', 'index.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'documents', 'contact.html'));
+});
+
+app.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+    console.log(`Received data: Name - ${name}, Email - ${email}, Message - ${message}`);
+
+    res.send('đã nhận!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server đang chạy trên cổng ${port}`);
+});
